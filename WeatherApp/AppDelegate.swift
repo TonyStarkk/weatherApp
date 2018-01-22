c97ba6044a833b8ac2ac89d86878d8546af8ed51
TonyStarkk <mohamed.ali.benyaagoub@gmail.com>
2018-01-22 06:44:07 +0100

    Initial Commit

---
+++ WeatherApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WeatherApp
+//
+//  Created by Mohamed Ali on 22/01/2018.
+//  Copyright © 2018 Mohamed Ali. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Mohamed Ali on 22/01/2018.
+//  Copyright © 2018 Mohamed Ali. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
